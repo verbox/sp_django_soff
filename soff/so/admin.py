4727068e99b4a6836ef6f5d6c5c9b826c29118b5
vbx <poap@interia.pl>
2012-05-06 17:18:22 +0200

    Dodano kolejne modele (zamówienie, zamówienie-danie, danie).

--- soff/so/admin.py
+++ soff/so/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-from so.models import Table
+from so.models import *
 admin.site.register(Table)
+admin.site.register(Dish)
+admin.site.register(DishEntry)
+admin.site.register(FoodOrder)
--- soff/so/models.py
+++ soff/so/models.py
@@ -1,5 +1,31 @@
+#-*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
+#Klasa reprezentująca pojedynczy stolik
 class Table(models.Model):
     clientsCount = models.IntegerField(default=0)
     maxClientsCount = models.IntegerField(default=4)
+
+#User = pracownik
+
+#Zamowienie, skladajace sie z listy wpisow, kelnera (pracownika-Usera),
+#stolika i ewentualnego komentarzu do zamowienia
+#Z pol "obslugowych"
+class FoodOrder(models.Model):
+    #lista wpisow w zamowieniu - z automatu
+    waiter = models.ForeignKey(User)
+    table = models.ForeignKey(Table)
+    comment = models.TextField()
+
+#Reprezentuje żarcie - tj. dane dotyczące pojedynczego dania   
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    addedDate = models.DateField(auto_now_add=True)
+    information = models.TextField()
+    
+#Wpis w zamowieniu - para Danie-il.sztuk
+class DishEntry(models.Model):
+    dish = models.ForeignKey(Dish)
+    foodOrder = models.ForeignKey(FoodOrder)
+    count = models.IntegerField(default=1)
