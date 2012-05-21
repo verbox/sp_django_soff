65e27bb25dee33aa28ca1dbaed818bf2b8bf9ac6
vbx <poap@interia.pl>
2012-05-21 21:12:52 +0200

    Dodano ProtectedListView (widok generyczny z dodanym zabezpieczeniem przed dostępem osób niezalogowanych).

--- soff/so/urls.py
+++ soff/so/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from utils import ProtectedListView
+from models import *
 urlpatterns = patterns('soff.so.views',
-   
-    url(r'^login$','login'),                   
-    url(r'^', 'start'),
 
+    url(r'^login$','login'),                   
+    url(r'^dish',ProtectedListView.as_view(queryset=Dish.objects.all(),
+                                           template_name="dishes.html" )),
+    url(r'^', 'start'),
 )
---
+++ soff/so/utils.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+
+#klasa na potrzeby zabezpieczenia widokow generycznych
+class ProtectedListView(ListView):
+    @method_decorator(login_required(login_url='/so/login'))
+    def dispatch(self, *args, **kwargs):
+        return super(ListView, self).dispatch(*args, **kwargs)
